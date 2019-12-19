fe09ef6fbe272cfce4afe746248ec32294bc472a
Kai Rabien <krabien@gmail.com>
2019-12-19 16:07:30 +0100

    Update module.js

--- module.js
+++ module.js
@@ -6,9 +6,5 @@ Sentry.init({ dsn: 'https://01edf29d06b94a159a5f7b762d18f811@sentry.io/1860656' 
 Sentry.captureMessage('hello from B4A cloud code');
 
 module.exports.test = () => { 
-  return 'hello dear world';
-};
-
-module.exports.test2 = () => { 
-  return {message: 'hello my dear world'};
+  return { time: moment() };
 };
