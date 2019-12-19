3024c973403d21e9086bd61b283127cc403fa12a
Kai Rabien <krabien@gmail.com>
2019-12-19 15:12:54 +0100

    Update module.js

--- module.js
+++ module.js
@@ -3,5 +3,6 @@ const uuidv1 = require('uuid/v1');
 
 const Sentry = require('@sentry/node');
 Sentry.init({ dsn: 'https://01edf29d06b94a159a5f7b762d18f811@sentry.io/1860656' });
+Sentry.captureMessage('hello from B4A cloud code');
 
-module.exports.hello = 'hello world';
+module.exports.hello = 'hello again!';
