abbd5444f828df4b5b6c42f10985ccc285796d27
Kai Rabien <krabien@gmail.com>
2019-12-19 15:09:11 +0100

    Update module.js

--- module.js
+++ module.js
@@ -1,1 +1,7 @@
+const moment  = require('moment');
+const uuidv1 = require('uuid/v1');
+
+const Sentry = require('@sentry/node');
+Sentry.init({ dsn: 'https://01edf29d06b94a159a5f7b762d18f811@sentry.io/1860656' });
+
 module.exports.hello = 'hello world';
