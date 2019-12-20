ec9cab7c20b7e1676ab09a86847dd229914515ca
Kai Rabien <krabien@gmail.com>
2019-12-20 09:36:01 +0100

    Update module.js

--- module.js
+++ module.js
@@ -1,5 +1,13 @@
-module.exports.hello = 'hello from github!';
+const moment  = require('moment');
+const uuidv1 = require('uuid/v1');
 
-module.exports.test = () => {
-    return { a: 'message from github' };
+const Sentry = require('@sentry/node');
+Sentry.init({ dsn: 'https://01edf29d06b94a159a5f7b762d18f811@sentry.io/1860656' });
+
+module.exports.test = () => { 
+  return {
+    a: '1',
+    b: moment().format(),
+    c: uuidv1() 
+  };
 };
